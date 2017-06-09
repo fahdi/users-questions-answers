425097ad04f03edb17d4715dd9bb48b3600ecbcc
Fahad Murtaza <fahdim@gmail.com>
2017-06-09 16:36:28 +0500

    Adding a convict based configuration settings file. A development.json file can update the defaults

---
+++ config/index.js
@@ -0,0 +1,32 @@
+const convict = require('convict');
+
+// Define a schema
+const conf = convict({
+  env: {
+    doc: 'The applicaton environment.',
+    format: ['development', 'production', 'test'],
+    default: 'development',
+    env: 'NODE_ENV'
+  },
+  port: {
+    doc: 'The Dashboard port to bind.',
+    format: 'port',
+    default: 3000,
+    env: 'PORT'
+  },  
+  mongodb: {
+    doc: 'The mongodb connection string.',
+    format: '*',
+    default: 'mongodb://localhost:27017/mongoosetest',
+    env: 'MONGODB'
+  }
+});
+
+// Load environment dependent configuration
+const env = conf.get('env');
+conf.loadFile(`./config/${env}.json`);
+
+// Perform validation
+conf.validate({allowed: 'strict'});
+
+module.exports = conf;
